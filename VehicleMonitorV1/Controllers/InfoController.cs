c2c1ffd8eb4d34ded00cb23efc5223d292000f1e
Huiben Zhang <n9074546@qut.edu.au>
2017-07-06 16:19:22 +1000

    add info controller

---
+++ VehicleMonitorV1/Controllers/InfoController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VehicleMonitorV1.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        /*
+        public ActionResult Index()
+        {
+            return View();
+        }*/
+        public string Index()
+        {
+            return "TEST";
+        }
+    }
+}
