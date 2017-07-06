22f32df26ad27ad6a70ffa65d8b34a64fd178da7
Huiben Zhang <n9074546@qut.edu.au>
2017-07-06 16:16:04 +1000

    new controller

--- VehicleMonitorV1/Controllers/VehicleMointorv1InfoController.cs
+++ VehicleMonitorV1/Controllers/VehicleMointorv1InfoController.cs
@@ -9,9 +9,14 @@ namespace VehicleMonitorV1.Controllers
     public class VehicleMointorv1InfoController : Controller
     {
         // GET: VehicleMointorv1Info
+        /*
         public ActionResult Index()
         {
             return View();
+        }*/
+        public String Index()
+        {
+            return "I am doing this project as a web server to display vehicle position data in real time";
         }
     }
 }
