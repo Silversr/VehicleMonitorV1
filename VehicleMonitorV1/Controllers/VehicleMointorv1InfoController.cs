58c1a6cedd78622b543eb27c154abb2f6d2d6cb3
Huiben Zhang <n9074546@qut.edu.au>
2017-07-06 14:59:26 +1000

    First

---
+++ VehicleMonitorV1/Controllers/VehicleMointorv1InfoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VehicleMonitorV1.Controllers
+{
+    public class VehicleMointorv1InfoController : Controller
+    {
+        // GET: VehicleMointorv1Info
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
